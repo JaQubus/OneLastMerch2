4a52b90cca3dfeddd879ebfa5052e7df9d98a010
Jakub <jakubkulik20@gmail.com>
2025-02-28 09:14:08 +0100

    Added css and now works on default django login and register templates

--- OneLastMerch/olm_auth/forms.py
+++ OneLastMerch/olm_auth/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-# from .models import CustomUser
+from .models import User
+
 
 # basic Django register form
 class RegisterForm(UserCreationForm):
-    # email = forms.EmailField()
+    email = forms.EmailField()
 
-    # class Meta:
-    #     model = CustomUser
-    #     fields = ["username", "email", "password1", "password2"]
-    pass
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
 
 class LoginForm(AuthenticationForm):
     pass
