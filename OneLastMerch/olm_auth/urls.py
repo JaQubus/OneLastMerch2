760b3aebfe0c8f416378843a67ba7c86d8dfbff2
Kuba <jakubkulik20@gmail.com>
2025-03-02 13:06:32 +0100

    Added default password change. This project is much quicker this way. I think it was a mistake not to use the provided forms or views from django. Added new css restyled all pages. The main improvements compared to olm is style and forms

--- OneLastMerch/olm_auth/urls.py
+++ OneLastMerch/olm_auth/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import olm_register, olm_login, olm_logout
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("register/", olm_register, name="register"),
     path("login/", olm_login, name="login"),
     path("logout/", olm_logout, name="logout"),
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='ui_ux/change_password.html'), name='change_password'),
+    path('change-password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='ui_ux/change_password_done.html'), name='password_change_done'),
 ]
